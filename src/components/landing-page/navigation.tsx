21ac6961401b719b2eb8be76ff36494c96ddcddd
wqeqwedq <13645594503@163.com>
2026-05-11 14:15:05 +0800

    4

--- src/components/landing-page/navigation.tsx
+++ src/components/landing-page/navigation.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import type { User } from "@supabase/supabase-js";
-import Logo from "@/components/logo";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
@@ -38,55 +37,12 @@ const NavItemsRight = ({ user }: { user: User | null }) => {
     </>
   );
 };
-const NavItemsLeft = () => {
-  const homeT = useTranslations("home.navigtion");
-  return (
-    <>
-      <Link
-        href={"#features"}
-        className="text-sm font-medium hover:underline underline-offset-4"
-      >
-        {homeT("features")}
-      </Link>
-      <Link
-        href={"#pricing"}
-        className="text-sm font-medium hover:underline underline-offset-4"
-      >
-        {homeT("pricing")}
-      </Link>
-      <Link
-        href={"#faqs"}
-        className="text-sm font-medium hover:underline underline-offset-4"
-      >
-        {homeT("faqs")}
-      </Link>
-      <Link
-        href={"https://github.com/geallenboy"}
-        className="text-sm font-medium hover:underline underline-offset-4"
-      >
-        {homeT("aboutMe")}
-      </Link>
-      <Link
-        href={"https://github.com/geallenboy/ai-image"}
-        className="text-sm font-medium hover:underline underline-offset-4 tracking-tighter"
-      >
-        {homeT("github")}
-      </Link>
-    </>
-  );
-};
 
 const Navigtion = async ({ user }: { user: User | null }) => {
   return (
     <div className="w-full bg-background/60 backdrop-blur-md fixed top-0 px-8 py-4 z-50 shadow-xl overflow-hidden">
       <header className="contariner mx-auto flex items-center ">
-        <div className="w-full flex items-center justify-between">
-          <div className="flex items-center justify-center gap-6">
-            <Logo />
-            <nav className="hidden md:flex items-center justify-center gap-3 ml-2">
-              <NavItemsLeft />
-            </nav>
-          </div>
+        <div className="w-full flex items-center justify-end">
           <div className="hidden md:flex items-center justify-center gap-3">
             <NavItemsRight user={user} />
           </div>
@@ -99,7 +55,6 @@ const Navigtion = async ({ user }: { user: User | null }) => {
             <SheetContent>
               <SheetTitle className="sr-only">导航</SheetTitle>
               <nav className="flex flex-col gap-4 mt-12">
-                <NavItemsLeft />
                 <NavItemsRight user={user} />
               </nav>
             </SheetContent>
